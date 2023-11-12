02954b6c94c96719654cea5e67c75dfa75016700
SenchaBka <aburiak@my.centennialcollege.ca>
2023-11-11 21:48:51 -0500

    Remove not used code

--- routes/index.js
+++ routes/index.js
@@ -1,29 +1,8 @@
-/*index.js - Arsenii Buriak - 301266501 - 01/10/23 */
-
-/* Creating a router */
 var express = require('express')
 var router = express.Router()
 
-
-/* Rendering pages */
 router.get('/', (req, res) => {
     res.render('home')
-})
-  
-router.get('/about', (req, res) => {
-    res.render('about')
-})
-  
-router.get('/projects', (req, res) => {
-    res.render('projects')
-})
-  
-router.get('/services', (req, res) => {
-    res.render('services')
-})
-  
-router.get('/contact', (req, res) => {
-    res.render('contact')
 })
   
 module.exports = router
