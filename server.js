c61c647dd5a4b90ddc629bf0c15f977c6866508e
SenchaBka <aburiak@my.centennialcollege.ca>
2023-11-11 21:06:01 -0500

    Initial commit with basic structure

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+
+app.use(express.static('public/stylesheets'))
+app.use(express.static('public/images'))
+
+const indexRouter = require('./routes/index')
+app.use('/', indexRouter)
+
+app.listen(3000)
+console.log('Server running at http://localhost:3000/')
